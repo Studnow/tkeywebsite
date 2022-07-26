b992acaa0ebea64ee03053361df9323c756e54f1
Igor <silent659@gmail.com>
2022-07-26 18:56:46 +0300

    return to first version form, changed hidden input value, some submit function fields

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,7 @@ import "@kingshott/iodine";
 
 window.Alpine = Alpine;
 
-Alpine.data("contactForm", form);
+Alpine.data("contactForm", validator);
 // Alpine.data("servicesForm", validator);
 
 // console.log(validator)
@@ -23,92 +23,92 @@ Alpine.start();
 
 // console.log(getContact);
 
-// function validator() {
-//   return {
-//     fields: {
-//       name: {
-//         value: null,
-//         rules: ["required", "string", "minLength:2"],
-//         validate(callback) {
-//           let { isValid, errorMsg } = callback(this);
-//           this.isValid = isValid;
-//           this.errorMsg = errorMsg;
-//         },
-//         isValid: null,
-//         errorMsg: null,
-//       },
-//       email: {
-//         value: null,
-//         rules: ["required", "email"],
-//         validate(callback) {
-//           let { isValid, errorMsg } = callback(this);
-//           this.isValid = isValid;
-//           this.errorMsg = errorMsg;
-//         },
-//         isValid: null,
-//         errorMsg: null,
-//       },
-//     },
-//     isFormValid: true,
-//     validationCallback(field) {
-//       Iodine.setErrorMessage("required", "Это обязательное поле");
-//       Iodine.setErrorMessage("minLength", "Имя должно содержать '[PARAM]' или больше буквы");
-//       Iodine.setErrorMessage("email", "Введите действующий адрес электронной почты. Например: address@mail.com");
-//       let { value, rules } = field;
-//       let isValid = Iodine.assert(value, rules).valid;
-//       let errorMsg = Iodine.assert(value, rules).valid ? null : Iodine.assert(value, rules).error;
-
-//       return { isValid, errorMsg };
-//     },
-//     submit() {
-//       this.isFormValid = !Object.values(this.fields).some((field) => !field.isValid);
-//       let myForm = document.getElementById("heroForm");
-//       let formData = new URLSearchParams(new FormData(myForm)).toString();
-//       console.log(formData);
-//       if (this.isFormValid) {
-//         fetch("/", {
-//           method: "POST",
-//           headers: { "Content-Type": "application/x-www-form-urlencoded" },
-//           body: formData,
-//         })
-//           // This is how we route to /thanks on successful form submission
-//           // More on $router.push function: https://router.vuejs.org/guide/essentials/navigation.html
-//           .then((response) => {
-//             if (response.ok) {
-//               myForm.reset();
-//               console.log("form reset")
-//             } else {
-//               throw new Error(`Something went wrong: ${response.statusText}`)
-//             }
-//           })
-//           // .then(() => console.log("Form submitted"))
-//           .catch((error) => alert(error));
-//       }else {
-//         console.log("Form is not valid")
-//       }
-//       return this.isFormValid;
-//     },
-//   };
-// }
-
-// document.querySelector("form").addEventListener("submit", handleSubmit);
-
-function form() {
+function validator() {
   return {
     fields: {
-      
+      name: {
+        value: null,
+        rules: ["required", "string", "minLength:2"],
+        validate(callback) {
+          let { isValid, errorMsg } = callback(this);
+          this.isValid = isValid;
+          this.errorMsg = errorMsg;
+        },
+        isValid: null,
+        errorMsg: null,
+      },
+      email: {
+        value: null,
+        rules: ["required", "email"],
+        validate(callback) {
+          let { isValid, errorMsg } = callback(this);
+          this.isValid = isValid;
+          this.errorMsg = errorMsg;
+        },
+        isValid: null,
+        errorMsg: null,
+      },
     },
-    handleSubmit(e) {
+    isFormValid: true,
+    validationCallback(field) {
+      Iodine.setErrorMessage("required", "Это обязательное поле");
+      Iodine.setErrorMessage("minLength", "Имя должно содержать '[PARAM]' или больше буквы");
+      Iodine.setErrorMessage("email", "Введите действующий адрес электронной почты. Например: address@mail.com");
+      let { value, rules } = field;
+      let isValid = Iodine.assert(value, rules).valid;
+      let errorMsg = Iodine.assert(value, rules).valid ? null : Iodine.assert(value, rules).error;
+
+      return { isValid, errorMsg };
+    },
+    submit() {
+      this.isFormValid = !Object.values(this.fields).some((field) => !field.isValid);
       let myForm = document.getElementById(this.$refs.form.attributes.id.value);
-      // console.log(this.$refs.form.attributes.id);
-      let formData = new FormData(myForm);
-      fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: new URLSearchParams(formData).toString(),
-      })
-        .then(() => console.log("Form successfully submitted"))
-        .catch((error) => alert(error));
+      let formData = new URLSearchParams(new FormData(myForm)).toString();
+      console.log(formData);
+      if (this.isFormValid) {
+        fetch("/", {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: formData,
+        })
+          // This is how we route to /thanks on successful form submission
+          // More on $router.push function: https://router.vuejs.org/guide/essentials/navigation.html
+          .then((response) => {
+            if (response.ok) {
+              myForm.reset();
+              console.log("form reset")
+            } else {
+              throw new Error(`Something went wrong: ${response.statusText}`)
+            }
+          })
+          // .then(() => console.log("Form submitted"))
+          .catch((error) => alert(error));
+      }else {
+        console.log("Form is not valid")
+      }
+      return this.isFormValid;
     },
   };
 }
+
+// document.querySelector("form").addEventListener("submit", handleSubmit);
+
+// function form() {
+//   return {
+//     fields: {
+      
+//     },
+//     handleSubmit(e) {
+//       let myForm = document.getElementById(this.$refs.form.attributes.id.value);
+//       // console.log(this.$refs.form.attributes.id);
+//       let formData = new FormData(myForm);
+//       fetch("/", {
+//         method: "POST",
+//         headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//         body: new URLSearchParams(formData).toString(),
+//       })
+//         .then(() => console.log("Form successfully submitted"))
+//         .catch((error) => alert(error));
+//     },
+//   };
+// }
